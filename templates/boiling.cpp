c81c310f95a05c91e2518515c64006890ea1a87f
mint-tube <mmooyrs@gmail.com>
2026-08-25 13:11:07 +0300

    add macros

--- templates/boiling.cpp
+++ templates/boiling.cpp
@@ -3,13 +3,16 @@ using namespace std;
 using i64 = int64_t;
 using u64 = uint64_t;
 #define all(x) (x).begin(), (x).end()
+#define iota(N) for (int __i = N; __i > 0; --__i) 
 #ifdef DEBUG 
 #define ln endl
 #define print(arr) for (auto &rvi : rv) cout << rvi << ' '; cout << ln;
+#define flag(x) cout << "\x1b[31;1mFLAG:\x1b[0m " << x << ln
 #define files(...)
 #else
 #define ln '\n'
 #define print(...)
+#define flag(...)
 #define files(name) freopen(name ".in", "r", stdin); freopen(name ".out", "w", stdout)
 #endif
 [[maybe_unused]] constexpr int mod = 1e9 + 7;
