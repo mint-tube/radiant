33fcee0bae03d8361f29d44fd2aa91f5ae101d2e
mint-tube <mmooyrs@gmail.com>
2026-09-01 16:31:06 +0300

    update templates

--- templates/boiling.cpp
+++ templates/boiling.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 using i64 = int64_t;
 using u64 = uint64_t;
-#define all(x) (x).begin(), (x).end()
+#define print(arr) for (const auto &rvi : arr) cout << rvi << ' '; cout << ln;
 #ifdef DEBUG 
 #define ln endl
-#define print(arr) for (auto &rvi : arr) cout << rvi << ' '; cout << ln;
 #define flag(x) cout << "\x1b[31;1mFLAG:\x1b[0m " << x << ln
 #define files(...)
 #else
 #define ln '\n'
-#define print(...)
 #define flag(...)
 #define files(name) freopen(name ".in", "r", stdin); freopen(name ".out", "w", stdout)
 #endif
